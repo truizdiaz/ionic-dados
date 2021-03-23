60bc9d992bb7fce57e708efda6d80c08e0e3ffdd
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2021-03-23 10:18:19 -0300

    Finalizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  img1 = '../../assets/img/dice1.png';
+  img2 = '../../assets/img/dice2.png'
 
   constructor() {}
 
+  tirarDados() {
+   
+
+    const numero1 = Math.floor(Math.random() * 6) + 1;
+    const numero2 = Math.floor(Math.random() * 6) + 1;
+    this.img1 = '../../assets/img/dice' + numero1 + '.png';
+    this.img2 = '../../assets/img/dice' + numero2 + '.png';
+  }
 }
